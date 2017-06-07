d36ca75c7202336a7438d2e3e9f13297bd70d388
Alexei Rodriguez <alexei.rodriguez95@hotmail.com>
2017-06-06 19:43:19 -0600

    agregar rutas de sumar y funcionalidad

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/0301199500621', function(req, res, next){
+  var yo = [
+                {"nombre":"Alexei Rodriguez Bonilla"},
+                {"cuenta":"0301199500621"},
+                {"correo": "alexei.rodriguez95@hotmail.com"}
+              ];
+
+  res.json(yo);
+});
+
+router.get('/suma', function(req, res, next){
+  res.render('suma',{"txtNumero1":"", "txtNumero2":""});
+});
+
+var suma = [];
+
+router.post('/suma', function(req, res, next){
+
+  suma.push(Number(req.body.txtNumero1) + Number(req.body.txtNumero2));
+
+  var sm = suma;
+  var rtObject = {};
+  //rtObject.numero = req.body.numero1;
+  rtObject.sm = sm;
+
+  res.render('suma', rtObject);
+});
+
 module.exports = router;
